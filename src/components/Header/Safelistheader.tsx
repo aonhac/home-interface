5072120b6e7e97f2d3e157cdec8be6751c103417
aonhac <105034482+aonhac@users.noreply.github.com>
2022-09-19 19:37:56 +0700

    Update Safelistheader.tsx

--- src/components/Header/Safelistheader.tsx
+++ src/components/Header/Safelistheader.tsx
@@ -38,13 +38,13 @@ const Safelistheader: React.FC = () => {
                             <NavDropdown.Item href="/roadmap">{t('Roadmap')}</NavDropdown.Item>
                             <NavDropdown.Item href="/tokenomic">{t('Tokenomics')}</NavDropdown.Item>
                             <NavDropdown.Item href="">{t('')}</NavDropdown.Item>
-                            <NavDropdown.Item href="https://pancakeswap.finance/swap" target="_blank">{t('Exchange')}</NavDropdown.Item>
+                            <NavDropdown.Item href="https://pancakeswap.finance/swap?outputCurrency=0x4689Bf06862d03ab26b77bf53421a362AF80D802" target="_blank">{t('Exchange')}</NavDropdown.Item>
                         </NavDropdown>
                     </Li>
                     <Li>
                         <Nav.Link href="https://app.alaskainu.finance" target="_blank">{t('Staking Testnet')}</Nav.Link>
                     </Li>
-                    <LiButton href="https://www.pinksale.finance/launchpads?chain=BSC" target="_blank">
+                    <LiButton href="https://www.pinksale.finance/launchpad/0x14eE31a9BA498EE91FfD921F5fa677F777aCab66?chain=BSC" target="_blank">
                         {t('Alaska Inu Fairlaunch')}
                     </LiButton>
                     <Li>
